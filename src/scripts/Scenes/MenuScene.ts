e24e961a8e4cfb422821842a6bbde97ee773d449
Ctech 21K <ctech21k@Ctech-21Ks-iMac.local>
2022-05-04 17:50:26 -0400

    added button to menu screen and menu screen

--- src/scripts/Scenes/MenuScene.ts
+++ src/scripts/Scenes/MenuScene.ts
@@ -3,22 +3,23 @@ import WebFont from 'webfontloader';
 import { colors } from '../constants';
 
 export default class GameScene extends Phaser.Scene {
+  start;
   constructor() {
     super({ key: 'MenuScene' });
   }
 
   preload() {
-    this.load.image(
-      'menu-scene-text',
-      //new URL('../../assets/menu-scene-text.png, import.meta.url).href
-    );
+    this.load.image('menuscreen', new URL('../../assets/menuscreen.png', import.meta.url).href);
+    this.load.image('start', new URL('../../assets/start.png', import.meta.url).href);
+
   }
 
+  //add an object for the start button 
   create() {
 
     const gameWidth: number = this.game.config.width as number;
     const gameHeight: number = this.game.config.height as number;
-    this.add.image(gameWidth / 2, gameHeight / 2, 'menu-scene-text');
+    this.add.image(gameWidth / 2, gameHeight / 2, 'menuscreen');
     const textStyle = {
       fontFamily: 'Space Mono',
       fontSize: '32px',
@@ -26,25 +27,33 @@ export default class GameScene extends Phaser.Scene {
       fill: colors.white,
       align: 'center',
     }
-
-    WebFont.load({
-      custom: {
-        families: ['Space Mono'],
-      },
-      active: () => {
-        this.add
-          .text(
-            gameWidth / 2,
-            gameHeight * (2 / 3),
-            'You can change me in MenuScene.js',
-            textStyle
-          )
-          .setOrigin(0.5);
-      },
-    });
-
-    this.input.keyboard.on('keydown-SPACE', () => {
+    this.start = this.add.image(gameWidth - 716, gameHeight - 310, 'start').setScale(.5, .5);
+    console.log(this.start);
+    // this.start.setVisible(false);
+    this.start.setInteractive();
+    this.start.on('pointerdown', () => {
+      console.log('start');
       this.scene.start('GameScene');
     });
+
+    //  WebFont.load({
+    //     custom: {
+    //       families: ['Space Mono'],
+    //     },
+    //     active: () => {
+    //       this.add
+    //         .text(
+    //           gameWidth / 2,
+    //           gameHeight * (2 / 3),
+    //           'You can change me in MenuScene.js',
+    //           textStyle
+    //         )
+    //         .setOrigin(0.5);
+    //     },
+    //   });
+
+    // this.input.keyboard.on('keydown-SPACE', () => {
+    //   this.scene.start('GameScene');
+    // });
   }
 }
