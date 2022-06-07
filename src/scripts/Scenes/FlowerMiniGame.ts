c94d96e63d552c83d8377e12e908f07f217bd759
Ctech 21J <ctech21j@Ctech-21Js-iMac.local>
2022-06-07 17:30:11 -0400

    adjusted flower petal sizes/ switched out flower petals

--- src/scripts/Scenes/FlowerMiniGame.ts
+++ src/scripts/Scenes/FlowerMiniGame.ts
@@ -39,7 +39,7 @@ export default class FlowerMiniGame extends Phaser.Scene {
     this.setFlowerPetals();
     //this.load.image('stem', new URL('../../assets/stem.png', import.meta.url).href);
     // this.flowerpetal = this.add.image(640, 250, 'petal').setScale(.2, .2);
-    this.flowerbud = this.add.image(645, 300, 'flowerbud').setScale(1.3, 1.3);
+    this.flowerbud = this.add.image(625, 290, 'flowerbud').setScale(1.3, 1.3);
     // this.flowerpetal.rotation += 1;
     console.log(this.flowerpetals);
     // this.input.setDraggable(this.flowerpetals);
@@ -71,38 +71,50 @@ export default class FlowerMiniGame extends Phaser.Scene {
 
 
   setFlowerPetals() {
-    let x = 640;
-    let y = 250;
-    let r = 0;
+    let x1 = 620;
+    let y1 = 250;
+    let x2 = 530;
+    let y2 = 350;
+    let r = 1;
     for (let i = 0; i < this.numPetals; i++) {
 
       if (i === 1) {
-        console.log('hey 00: ' + x + ' ' + y, i);
-        r = 1;
-        x += 50;
-        y += 35;
+        console.log('hey 00: ' + x1 + ' ' + y1, i);
+        r = 2;
+        x1 += 50;
+        y1 += 35;
+        x2 -= 5;
+        y2 -= 105;
       }
 
       if (i === 2) {
-        console.log('hey 01', x, y, i);
-        r = 2;
-        x -= 15;
-        y += 50;
+        console.log('hey 01', x1, y1, i);
+        r = 3;
+        x1 -= 15;
+        y1 += 50;
+        x2 += 100;
+        y2 -= 60;
+
       }
       if (i === 3) {
-        console.log('hey 02', x, y, i);
-        r = 3.5;
-        x -= 50;
-        y -= 5;
+        console.log('hey 02', x1, y1, i);
+        r = 4.5;
+        x1 -= 50;
+        y1 -= 5;
+        x2 += 90;
+        y2 += 100;
+
       }
       if (i === 4) {
-        console.log('hey 03', x, y, i);
-        r = 5;
-        x -= 20;
-        y -= 50;
+        console.log('hey 03', x1, y1, i);
+        r = 6;
+        x1 -= 20;
+        y1 -= 50;
+        x2 -= 20;
+        y2 += 80;
       }
-      this.flowerpetals.create(x, y, 'petal').setScale(2.5, 2.5).rotation += r;
-      this.deadpetals.create(x, y, 'deadpetal').setScale(.9, .9).rotation += r;
+      this.flowerpetals.create(x1, y1, 'petal').setScale(2.5, 2.5).rotation += r;
+      this.deadpetals.create(x2, y2, 'deadpetal').setScale(.9, .9).rotation += r;
 
 
     }
